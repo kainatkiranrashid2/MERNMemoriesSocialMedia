a59b33e5415769755414d31fe6b81d448f314f02
KainatKiranRashid <kainatkiranrashid2@gmail.com>
2022-04-11 22:29:58 +0500

    .env file created and added to gitignore

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
-
+dotenv.config()
 const app = express()
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
@@ -13,7 +14,7 @@ app.use('/posts', postRoutes)
 
 
 
-const CONNECTION_URL= 'mongodb+srv://kainatkiranrashid:aminah1234@cluster0.7buc5.mongodb.net/memories?retryWrites=true&w=majority'
+const CONNECTION_URL= process.env.CONNECTION_URL
 const PORT = process.env.PORT || 5000
 mongoose.connect(CONNECTION_URL,{
     useNewUrlParser : true,
